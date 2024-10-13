7b6c92a2f5b0da8c2bb23bbc1d1404e02d542948
oojjrs <oojjrs@gmail.com>
2024-10-14 08:47:18 +0900

    네트워크용 패킷 serializer 추가

---
+++ Assets/NetDeserializer.cs
@@ -0,0 +1,179 @@
+﻿using System;
+#if CONCURRENT
+using System.Collections.Concurrent;
+#else
+using System.Collections.Generic;
+#endif
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+public static class NetDeserializer
+{
+#if CONCURRENT
+    private static readonly ConcurrentDictionary<Type, PropertyInfo[]> _cache = new();
+#else
+    private static readonly Dictionary<Type, PropertyInfo[]> _cache = new();
+#endif
+
+    public static object Deserialize(Stream stream)
+    {
+        // 스트림을 닫지 않기 위해 using하지 않음.
+        var reader = new BinaryReader(stream);
+        var name = reader.ReadString();
+        if (string.IsNullOrWhiteSpace(name))
+            return default;
+
+        var type = Type.GetType(name);
+        if (type == default)
+            return default;
+
+        return ReadClass(reader, type, GetProperties(type));
+    }
+
+    private static PropertyInfo[] GetProperties(Type type)
+    {
+        if (_cache.TryGetValue(type, out var value) == false)
+        {
+            value = type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty | BindingFlags.SetProperty).Where(t => t.CanRead && t.CanWrite).OrderBy(t => t.Name).ToArray();
+            _cache[type] = value;
+        }
+
+        return value;
+    }
+
+    private static bool IsItem(Type propertyType)
+    {
+        return propertyType.IsPrimitive || (propertyType == typeof(string)) || (propertyType == typeof(DateTime));
+    }
+
+    private static bool IsTuple(Type type)
+    {
+        if (type.IsGenericType == false)
+            return false;
+
+        var openType = type.GetGenericTypeDefinition();
+        return openType == typeof(ValueTuple<>)
+            || openType == typeof(ValueTuple<,>)
+            || openType == typeof(ValueTuple<,,>)
+            || openType == typeof(ValueTuple<,,,>)
+            || openType == typeof(ValueTuple<,,,,>)
+            || openType == typeof(ValueTuple<,,,,,>)
+            || openType == typeof(ValueTuple<,,,,,,>)
+            || (openType == typeof(ValueTuple<,,,,,,,>) && IsTuple(type.GetGenericArguments()[7]));
+    }
+
+    private static object ReadEnum(BinaryReader br, Type enumType)
+    {
+        var s = br.ReadString();
+        return Enum.Parse(enumType, s);
+    }
+
+    private static object ReadClass(BinaryReader br, Type type, PropertyInfo[] props)
+    {
+        var value = Activator.CreateInstance(type);
+        foreach (var prop in props)
+        {
+            if (br.BaseStream.Position >= br.BaseStream.Length)
+                break;
+
+            var isExist = br.ReadBoolean();
+            if (isExist == false)
+                continue;
+
+            if (prop.PropertyType.IsArray)
+            {
+                var elementType = prop.PropertyType.GetElementType();
+
+                var length = br.ReadInt32();
+                if (length <= 0)
+                {
+                    prop.SetValue(value, Array.CreateInstance(elementType, 0));
+                    continue;
+                }
+
+                var ret = Array.CreateInstance(elementType, length);
+                if (elementType.IsEnum)
+                {
+                    for (int i = 0; i < length; ++i)
+                        ret.SetValue(ReadEnum(br, elementType), i);
+                }
+                else if (IsItem(elementType))
+                {
+                    for (int i = 0; i < length; ++i)
+                        ret.SetValue(ReadItem(br, elementType), i);
+                }
+                else if (IsTuple(elementType))
+                {
+                    for (int i = 0; i < length; ++i)
+                        ret.SetValue(ReadTuple(br, elementType), i);
+                }
+                else
+                {
+                    var propProps = GetProperties(elementType);
+                    for (int i = 0; i < length; ++i)
+                        ret.SetValue(ReadClass(br, elementType, propProps), i);
+                }
+                prop.SetValue(value, ret);
+            }
+            else
+            {
+                if (prop.PropertyType.IsEnum)
+                    prop.SetValue(value, ReadEnum(br, prop.PropertyType));
+                else if (IsItem(prop.PropertyType))
+                    prop.SetValue(value, ReadItem(br, prop.PropertyType));
+                else if (IsTuple(prop.PropertyType))
+                    prop.SetValue(value, ReadTuple(br, prop.PropertyType));
+                else
+                    prop.SetValue(value, ReadClass(br, prop.PropertyType, GetProperties(prop.PropertyType)));
+            }
+        }
+
+        return value;
+    }
+
+    private static object ReadItem(BinaryReader br, Type type)
+    {
+        if (type == typeof(string))
+            return br.ReadString();
+        else if (type == typeof(float))
+            return br.ReadSingle();
+        else if (type == typeof(long))
+            return br.ReadInt64();
+        else if (type == typeof(int))
+            return br.ReadInt32();
+        else if (type == typeof(short))
+            return br.ReadInt16();
+        else if (type == typeof(byte))
+            return br.ReadByte();
+        else if (type == typeof(bool))
+            return br.ReadBoolean();
+        else if (type == typeof(double))
+            return br.ReadDouble();
+        else if (type == typeof(char))
+            return br.ReadChar();
+        else if (type == typeof(DateTime))
+            return DateTime.FromBinary(br.ReadInt64());
+        else
+            throw new NotImplementedException();
+    }
+
+    private static object ReadTuple(BinaryReader br, Type type)
+    {
+        var value = Activator.CreateInstance(type);
+        var fields = type.GetFields();
+        foreach (var field in fields)
+        {
+            if (field.FieldType.IsEnum)
+                field.SetValue(value, ReadEnum(br, field.FieldType));
+            else if (IsItem(field.FieldType))
+                field.SetValue(value, ReadItem(br, field.FieldType));
+            else if (IsTuple(field.FieldType))
+                field.SetValue(value, ReadTuple(br, field.FieldType));
+            else
+                field.SetValue(value, ReadClass(br, field.FieldType, GetProperties(field.FieldType)));
+        }
+
+        return value;
+    }
+}
---
+++ Assets/NetSerializer.cs
@@ -0,0 +1,172 @@
+﻿using System;
+#if CONCURRENT
+using System.Collections.Concurrent;
+#else
+using System.Collections.Generic;
+#endif
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+public static class NetSerializer
+{
+#if CONCURRENT
+    private static readonly ConcurrentDictionary<Type, PropertyInfo[]> _cache = new();
+#else
+    private static readonly Dictionary<Type, PropertyInfo[]> _cache = new();
+#endif
+
+    private static PropertyInfo[] GetProperties(Type type)
+    {
+        if (_cache.TryGetValue(type, out var value) == false)
+        {
+            value = type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty | BindingFlags.SetProperty).Where(t => t.CanRead && t.CanWrite).OrderBy(t => t.Name).ToArray();
+            _cache[type] = value;
+        }
+
+        return value;
+    }
+
+    private static bool IsItem(Type propertyType)
+    {
+        return propertyType.IsPrimitive || propertyType.IsEnum || (propertyType == typeof(string)) || (propertyType == typeof(DateTime));
+    }
+
+    private static bool IsTuple(Type type)
+    {
+        if (type.IsGenericType == false)
+            return false;
+
+        var openType = type.GetGenericTypeDefinition();
+        return openType == typeof(ValueTuple<>)
+            || openType == typeof(ValueTuple<,>)
+            || openType == typeof(ValueTuple<,,>)
+            || openType == typeof(ValueTuple<,,,>)
+            || openType == typeof(ValueTuple<,,,,>)
+            || openType == typeof(ValueTuple<,,,,,>)
+            || openType == typeof(ValueTuple<,,,,,,>)
+            || (openType == typeof(ValueTuple<,,,,,,,>) && IsTuple(type.GetGenericArguments()[7]));
+    }
+
+    public static byte[] Serialize(object packet)
+    {
+        using (var ms = new MemoryStream())
+        {
+            using (var writer = new BinaryWriter(ms))
+            {
+                writer.Write(packet.GetType().FullName);
+                WriteClass(writer, packet, GetProperties(packet.GetType()));
+            }
+
+            return ms.ToArray();
+        }
+    }
+
+    private static void WriteClass(BinaryWriter bw, object value, PropertyInfo[] props)
+    {
+        foreach (var prop in props)
+        {
+            var propValue = prop.GetValue(value);
+            var isExist = propValue != null;
+            bw.Write(isExist);
+
+            if (isExist == false)
+                continue;
+
+            if (prop.PropertyType.IsArray)
+            {
+                var arr = (Array)propValue;
+                bw.Write(arr.Length);
+
+                if (arr.Length <= 0)
+                    continue;
+
+                var elementType = arr.GetType().GetElementType();
+                if (IsItem(elementType))
+                {
+                    for (int i = 0; i < arr.Length; ++i)
+                        WriteItem(bw, arr.GetValue(i));
+                }
+                else if (IsTuple(elementType))
+                {
+                    for (int i = 0; i < arr.Length; ++i)
+                        WriteTuple(bw, arr.GetValue(i));
+                }
+                else
+                {
+                    var propProps = GetProperties(elementType);
+                    for (int i = 0; i < arr.Length; ++i)
+                        WriteClass(bw, arr.GetValue(i), propProps);
+                }
+            }
+            else
+            {
+                if (IsItem(prop.PropertyType))
+                    WriteItem(bw, propValue);
+                else if (IsTuple(prop.PropertyType))
+                    WriteTuple(bw, propValue);
+                else
+                    WriteClass(bw, propValue, GetProperties(prop.PropertyType));
+            }
+        }
+    }
+
+    private static void WriteItem(BinaryWriter bw, object o)
+    {
+        if (o.GetType().IsEnum)
+        {
+            bw.Write(o.ToString());
+            return;
+        }
+
+        switch (o)
+        {
+            case string value:
+                bw.Write(value);
+                break;
+            case float value:
+                bw.Write(value);
+                break;
+            case long value:
+                bw.Write(value);
+                break;
+            case int value:
+                bw.Write(value);
+                break;
+            case short value:
+                bw.Write(value);
+                break;
+            case byte value:
+                bw.Write(value);
+                break;
+            case bool value:
+                bw.Write(value);
+                break;
+            case double value:
+                bw.Write(value);
+                break;
+            case char value:
+                bw.Write(value);
+                break;
+            case DateTime value:
+                bw.Write(value.ToBinary());
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    private static void WriteTuple(BinaryWriter bw, object o)
+    {
+        foreach (var field in o.GetType().GetFields())
+        {
+            var fieldValue = field.GetValue(o);
+            if (IsItem(field.FieldType))
+                WriteItem(bw, fieldValue);
+            else if (IsTuple(field.FieldType))
+                WriteTuple(bw, fieldValue);
+            else
+                WriteClass(bw, fieldValue, GetProperties(field.FieldType));
+        }
+    }
+}
