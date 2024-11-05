66fa54c1a8654ba5d763822fc88a9ae36fecb0bf
oojjrs <oojjrs@gmail.com>
2024-11-05 11:47:01 +0900

    namespace ;;;

---
+++ Assets/MyEquatableObjectT.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public abstract class MyEquatableObjectT<T> : IEquatable<T> where T : MyEquatableObjectT<T>
+{
+    public abstract bool Equals(T other);
+
+    public override bool Equals(object obj)
+    {
+        return obj is T other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public static bool operator ==(MyEquatableObjectT<T> lhs, MyEquatableObjectT<T> rhs)
+    {
+        if (ReferenceEquals(lhs, rhs))
+            return true;
+
+        if ((lhs == default) || (rhs == default))
+            return false;
+
+        return lhs.Equals(rhs);
+    }
+
+    public static bool operator !=(MyEquatableObjectT<T> lhs, MyEquatableObjectT<T> rhs)
+    {
+        return !(lhs == rhs);
+    }
+}
