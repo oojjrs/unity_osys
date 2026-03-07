66346c8e751ff518d51d83633707bbaab9ff3acd
oojjrs <oojjrs@gmail.com>
2026-03-07 15:44:32 +0900

    XmlHelper 추가

---
+++ Assets/XmlHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+public static class XmlHelper
+{
+    public static void Build<T>(string text, Action<T[]> onLoaded)
+    {
+        using (var sr = new StringReader(text))
+        {
+            var xs = new XmlSerializer(typeof(T[]));
+            onLoaded?.Invoke((T[])xs.Deserialize(sr));
+        }
+    }
+}
