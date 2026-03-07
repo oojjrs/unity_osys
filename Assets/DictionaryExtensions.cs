45a631d816c28d35cc79c5ba5d0be67b7e2c361b
oojjrs <oojjrs@gmail.com>
2026-03-07 15:59:01 +0900

    DictionaryExtensions 추가

---
+++ Assets/DictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class DictionaryExtensions
+{
+    public static bool InsertAll<TKey, TValue>(this Dictionary<TKey, TValue> dic, IEnumerable<TValue> values, Func<TValue, TKey> getKey)
+    {
+        foreach (var value in values)
+        {
+            if (dic.TryAdd(getKey(value), value) == false)
+                return false;
+        }
+
+        return true;
+    }
+}
